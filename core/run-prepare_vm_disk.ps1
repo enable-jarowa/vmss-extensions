eebdb35768bfea28249354d635f18541f04b1b42
Lorenz Hänggi <lorenz.haenggi@jarowa.ch>
2021-03-05 18:34:12 +0100

    update script

--- core/run-prepare_vm_disk.ps1
+++ core/run-prepare_vm_disk.ps1
@@ -1,26 +1,26 @@
 Write-Host "----------------------------------------------------------"
-Write-Host "Start prepare script in location '$($PSScriptRoot)'"
+Write-Host "$(Get-Date) - Start prepare script in location '$($PSScriptRoot)'"
 Write-Host "----------------------------------------------------------"
 
 function DownloadAndSaveScript([String]$scriptUrl, [String]$scriptName, [String] $folderName) {
     $scriptFullName = "$($folderName)\$($scriptName)"
 
-    Write-Host "download from    : $($scriptUrl)"
-    Write-Host "script name      : $($scriptName)"
-    Write-Host "folder name      : $($folderName)"
-    Write-Host "script full name : $($scriptFullName)"
+    Write-Host "$(Get-Date) - download from    : $($scriptUrl)"
+    Write-Host "$(Get-Date) - script name      : $($scriptName)"
+    Write-Host "$(Get-Date) - folder name      : $($folderName)"
+    Write-Host "$(Get-Date) - script full name : $($scriptFullName)"
 
     mkdir $folderName -ea Ignore
     cd $folderName
-    Write-Host "changed to folder : $($folderName)"
+    Write-Host "$(Get-Date) - changed to folder : $($folderName)"
 
-    Write-Host "start downloading from '$($scriptUrl)'"
+    Write-Host "$(Get-Date) - start downloading from '$($scriptUrl)'"
     (Invoke-WebRequest -UseBasicParsing -Uri "$($scriptUrl)").Content | Out-File -FilePath "$($scriptFullName)"
-    Write-Host "successfully saved to '$($scriptFullName)'"
+    Write-Host "$(Get-Date) - successfully saved to '$($scriptFullName)'"
 
-    Write-Host "start powershell with command: 'powershell.exe -ExecutionPolicy Unrestricted -windowstyle hidden -noninteractive -nologo -file $($scriptFullName)'"
+    Write-Host "$(Get-Date) - start powershell with command: 'powershell.exe -ExecutionPolicy Unrestricted -windowstyle hidden -noninteractive -nologo -file $($scriptFullName)'"
     powershell.exe -ExecutionPolicy Unrestricted -windowstyle hidden -noninteractive -nologo -file "$($scriptFullName)"
-    Write-Host "executed"
+    Write-Host "$(Get-Date) - executed"
 }
 
 DownloadAndSaveScript "https://raw.githubusercontent.com/enable-jarowa/vmss-extensions/main/core/prepare_vm_disk.ps1" `
