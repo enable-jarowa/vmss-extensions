973f410d22e4a1e7d985f679631c9c8d61278aed
Lorenz Hänggi <77002157+lorenz-haenggi-jarowa@users.noreply.github.com>
2021-03-07 13:47:22 +0100

    Update hello-vmss.ps1

--- hello/hello-vmss.ps1
+++ hello/hello-vmss.ps1
@@ -4,6 +4,8 @@ Write-Host "------------------------------------------"
 Write-Host "$($args.Count) received"
 Write-Host "------------------------------------------"
 Write-Host "Hello Vmss"
+Write-Host "Nof arguments: $($args.Count)"
+## dont do this if you pass secure arguments
 Write-Host "Arguments: $($args)"
 Write-Host "------------------------------------------"
 Write-Host "done"
