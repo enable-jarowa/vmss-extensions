383c2f35c1305fafa2f64067a378620711cd48c3
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-03-19 13:48:38 +0100

    upload fix vm script

---
+++ vm/fix-vm-start-params.bat
@@ -0,0 +1,24 @@
+set D=%~dp0
+echo %D%
+
+echo "===================================================" >> %D%set-netsh-log.txt
+echo "dynamic ports before" >> %D%set-netsh-log.txt
+netsh int ipv4 show dynamicport tcp >> %D%set-netsh-log.txt
+netsh int ipv4 set dynamicport tcp start=1025 num=64511 store=persistent
+netsh int ipv6 set dynamicport tcp start=1025 num=64511 store=persistent
+
+echo "add tcp delay 15s to registry" >> %D%set-netsh-log.txt
+
+regedit /s %D%tcp-timed-wait-delay.reg
+
+date /T >> %D%set-netsh-log.txt
+time /T >> %D%set-netsh-log.txt
+echo "netsh set to 10000" >> %D%set-netsh-log.txt
+netsh int ipv4 show dynamicport tcp >> %D%set-netsh-log.txt
+echo "===================================================" >> %D%set-netsh-log.txt
+
+GOTO ende
+
+:endparse
+echo "call script with directory as parameter"
+:ende
