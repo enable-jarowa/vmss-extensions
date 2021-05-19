f941d26e70b5b9d2095ca70289d27ac53e0bb3f8
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-05-19 13:22:00 +0200

    adapt installation of SF 7.2

--- core/install-sfsdk.ps1
+++ core/install-sfsdk.ps1
@@ -14,14 +14,16 @@ $f_featurearray = $f_features.ToLower().Split(",").Trim().Where({ $_ -ne "" });
 if ($f_featurearray.Contains("sfsdk")) {
      Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Scope CurrentUser 
 
-    $msi = "MicrosoftServiceFabricSDK.4.2.477.msi"
+    $msi = "WebPlatformInstaller_x64_en-US.msi"
     $fileDownloaded = "$($env:TEMP)\$($msi)"
     if (!(Test-Path $fileDownloaded -PathType leaf)) {
         Invoke-WebRequest `
-            -Uri "https://download.microsoft.com/download/f/c/3/fc39707f-f67c-4c1b-9274-a055a3eb51b8/MicrosoftServiceFabricSDK.4.2.477.msi" `
+            -Uri "https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x64_en-US.msi" `
             -OutFile $fileDownloaded -UseBasicParsing
     }
     Start-Process "msiexec" -ArgumentList '/i', "$($fileDownloaded)", '/passive', '/quiet', '/norestart', '/qn' -NoNewWindow -Wait; 
+
+    Start-Process "$($env:programfiles)\microsoft\web platform installer\WebPICMD.exe" -ArgumentList '/Install', '/Products:ServiceFabricSDK_4_2_CU7,ServiceFabricRuntime_7_2_CU7', '/AcceptEULA' -NoNewWindow -Wait
 
 }
 
