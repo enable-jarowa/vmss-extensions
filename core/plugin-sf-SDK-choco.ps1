a119b752d14f9271e5292ee890335920a4336d4b
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-05-27 23:01:35 +0200

    update choco script

--- core/plugin-sf-SDK-choco.ps1
+++ core/plugin-sf-SDK-choco.ps1
@@ -8,6 +8,25 @@ if (!(Test-Path $logpath)) {
     mkdir "C:\Windows\system32\config\systemprofile\AppData\Local\Microsoft\Web Platform Installer\logs\install\"
 }
 
+
+Write-Output "Installing /webpicmd"
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Scope CurrentUser 
+$logpath="C:\Windows\system32\config\systemprofile\AppData\Local\Microsoft\Web Platform Installer\logs\install\"
+if (!(Test-Path $logpath)) {
+    mkdir "C:\Windows\system32\config\systemprofile\AppData\Local\Microsoft\Web Platform Installer\logs\install\"
+}
+
+$msi = "WebPlatformInstaller_x64_en-US.msi"
+$fileDownloaded = "$($env:TEMP)\$($msi)"
+if (!(Test-Path $fileDownloaded -PathType leaf)) {
+    Invoke-WebRequest `
+        -Uri "https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x64_en-US.msi" `
+        -OutFile $fileDownloaded -UseBasicParsing
+}
+Start-Process "msiexec" -ArgumentList '/i', "$($fileDownloaded)", '/passive', '/quiet', '/norestart', '/qn' -NoNewWindow -Wait; 
+
+
 Write-Output "Installing /choco"
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
