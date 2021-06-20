3e4317abea8cda4e5ef29d154d16c592b48fe2f3
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-06-20 22:01:14 +0200

    run plugins

---
+++ core/run-log-plugins.ps1
@@ -0,0 +1,21 @@
+## JAROWA AG, Industriestrasse 47, 6300 Zug
+## owner: lorenz-haenggi-jarowa
+## this script is used to prepare a download software and install it
+## based on a plugin-mechanism
+## 
+## download "plugins-execute.ps1"
+## download all plugins and install them
+## parameters: 6 (1=1 2=2 3=3 4=4 5=5 6="feature1,feature2,...")
+## featurs
+## - sfstandalone
+## example: powershell.exe .\run-log-plugins.ps1 1 2 3 4 5 "sfstandalone"
+
+$filenamePlugin = "plugins-execute";
+$psfilePlugin = "$($filenamePlugin).ps1";
+$fileDownloadedPlugin = "$($env:TEMP)\$($psfilePlugin)"
+Invoke-WebRequest `
+    -Uri "https://raw.githubusercontent.com/enable-jarowa/vmss-extensions/main/core/$($psfilePlugin)" `
+    -OutFile $fileDownloadedPlugin -UseBasicParsing
+
+. $env:TEMP\$psfilePlugin @args *>> "$($env:TEMP)\$($filenamePlugin).log"
+
