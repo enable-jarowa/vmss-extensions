d2bbd919a8fc787286c1346e615779d449c8607b
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-07-07 18:28:28 +0200

    update for config-tool and remove *

--- core/plugin-sf-network.ps1
+++ core/plugin-sf-network.ps1
@@ -13,6 +13,18 @@ if ($null -eq $enableRule) {
     Write-Host "$($ruleName) - Rule already exists"
 }
 
+$ruleName = "_Enable Service Fabric App Config Ports"
+$enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
+if ($null -eq $enableRule) {
+    New-NetFirewallRule -DisplayName '_Enable Service Fabric App Config Ports' `
+        -Profile @('Domain', 'Private', 'Public') `
+        -Direction Inbound -Action Allow -Protocol TCP `
+        -LocalPort @('8020')
+} else {
+    Write-Host "$($ruleName) - Rule already exists"
+}
+
+
 Write-Output "------------------------------------------"
 Write-Output "done"
 Write-Output "------------------------------------------"
