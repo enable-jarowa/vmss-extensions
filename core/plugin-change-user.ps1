aaa1e43ab3945ddd80eccf9384f4d84ef3773adf
Lorenz Hänggi <77002157+lorenz-haenggi-jarowa@users.noreply.github.com>
2021-09-01 21:45:28 +0200

    Update plugin-change-user.ps1

--- core/plugin-change-user.ps1
+++ core/plugin-change-user.ps1
@@ -3,7 +3,7 @@ Write-Output "Custom script: plugin-change-user.ps1"
 Write-Output "------------------------------------------"
 
 Write-Output "Change master user - password never expirer"
-Get-LocalUser -Name master | Set-LocalUser -PasswordNeverExpires $True
+Get-LocalUser -Name master | Set-LocalUser -PasswordNeverExpires $True -ErrorAction Ignore
 
 Write-Output "------------------------------------------"
 Write-Output "done"
