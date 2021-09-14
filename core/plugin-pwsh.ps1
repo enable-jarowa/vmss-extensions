a36b79399627c96361bc6ac3c1dc713fdd066adc
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-09-14 21:51:22 +0200

    fix pwsh script

--- core/plugin-pwsh.ps1
+++ core/plugin-pwsh.ps1
@@ -16,32 +16,30 @@ $f_featurearray = $f_features.ToLower().Split(",").Trim().Where({ $_ -ne "" });
 ## https://github.com/PowerShell/PowerShell/releases/tag/v7.1.4
 
 if ($f_featurearray.Contains("msbuildtools")) {
-
-    $msiVersion = "7.1.4"
-    $msi = "PowerShell-$($msiVersion)-win-x64.zip"
-    $fileDownloaded = "$($env:TEMP)\$($msi)"
-    if (!(Test-Path $fileDownloaded -PathType leaf)) {
-        Invoke-WebRequest `
-            -Uri "https://github.com/PowerShell/PowerShell/releases/download/v$($msiVersion)/$($msi)" `
-            -OutFile $fileDownloaded
-    }
     $location="C:\Program Files\PowerShell\7"
     if (!(Test-Path $location\pwsh.exe -PathType leaf)) {
+
+        $msiVersion = "7.1.4"
+        $msi = "PowerShell-$($msiVersion)-win-x64.zip"
+        $fileDownloaded = "$($env:TEMP)\$($msi)"
+        if (!(Test-Path $fileDownloaded -PathType leaf)) {
+            Invoke-WebRequest `
+                -Uri "https://github.com/PowerShell/PowerShell/releases/download/v$($msiVersion)/$($msi)" `
+                -OutFile $fileDownloaded
+        }
         mkdir -p "$($location)"
         Expand-Archive $fileDownloaded "$($location)" -Force
-
-        $newPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
-        if (!$newPath.Contains("PowerShell")) {
-            $newPath += ";$($location)"
-            $newPath = [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
-            Write-Output "added pwsh to path"
-        } else {
-            Write-Output "pwsh is already in path"
-        }
     } else {
-            Write-Output "pwsh already installed"
+        Write-Output "pwsh already installed"
     }
-
+    $newPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+    if (!$newPath.Contains("PowerShell\7")) {
+        $newPath += ";$($location)"
+        $newPath = [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+        Write-Output "added pwsh to path"
+    } else {
+        Write-Output "pwsh is already in path"
+    }
 }
 
 
