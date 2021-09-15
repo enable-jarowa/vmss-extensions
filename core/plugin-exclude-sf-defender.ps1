9c03b2383f2f9be770ff640ebaf2fcec876025f4
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-09-15 09:21:11 +0200

    fix strange exclusion bug

--- core/plugin-exclude-sf-defender.ps1
+++ core/plugin-exclude-sf-defender.ps1
@@ -2,25 +2,32 @@ Write-Output "------------------------------------------"
 Write-Output "Custom script: prepare-exclude-sf-defender.ps1"
 Write-Output "------------------------------------------"
 
-$allExclPaths = (Get-MpPreference).ExclusionPath
+try {
+    $allExclPaths = (Get-MpPreference).ExclusionPath
 
-$exclPaths = @(
-    "c:\agent", 
-    "c:\a", #build folder
-    "S:\", 
-    "D:\", 
-    "C:\Program Files\Microsoft Service Fabric"
-)
+    $exclPaths = @(
+        "c:\agent", 
+        "c:\a", #build folder
+        "S:\", 
+        "D:\", 
+        "C:\Program Files\Microsoft Service Fabric"
+    )
 
-for ($defenderI=0; $defenderI -lt $exclPaths.Count; $defenderI++) {
-    $exclPath = $exclPaths[$defenderI]
-    if (($null -eq $allExclPaths) -or (!$allExclPaths.Contains($exclPath))) {
-        Add-MpPreference -ExclusionPath $exclPath
-        Write-Host "$($exclPath) - exclusion added"
-    } else {
-        Write-Host "$($ruleName) - exclusion already done"
+    for ($defenderI=0; $defenderI -lt $exclPaths.Count; $defenderI++) {
+        $exclPath = $exclPaths[$defenderI]
+        if (($null -eq $allExclPaths) -or (!$allExclPaths.Contains($exclPath))) {
+            Add-MpPreference -ExclusionPath $exclPath
+            Write-Host "$($exclPath) - exclusion added"
+        } else {
+            Write-Host "$($ruleName) - exclusion already done"
+        }
     }
 }
+catch {
+    Write-Output "strange defender message but - not critial. ignore"
+    Write-Output "error message $($_.Exception.Message)"
+}
+
 
 Write-Output "------------------------------------------"
 Write-Output "done"
