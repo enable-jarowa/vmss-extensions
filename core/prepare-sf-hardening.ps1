33417947f4a2643c83f2c2e129991340a1019e10
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2021-11-22 14:40:17 +0100

    empty script for hardening

---
+++ core/prepare-sf-hardening.ps1
@@ -0,0 +1,22 @@
+Write-Output "------------------------------------------"
+Write-Output "Custom script: prepare-sf-hardening.ps1"
+Write-Output "------------------------------------------"
+Write-Output "$($args.Count) received"
+Write-Output "------------------------------------------"
+$f_url = $args[0]
+$f_folder = $args[1]
+$f_drive = $args[2]
+$f_account = $args[3]
+$f_key= $args[4]
+$f_features="$($args[5])"
+Write-Output "Features=$($f_features)"
+$f_featurearray = $f_features.ToLower().Split(",").Trim().Where({ $_ -ne "" });
+
+if ($f_featurearray.Contains("hardening")) {
+    
+}
+
+Write-Output "------------------------------------------"
+Write-Output "prepare-sf-hardening.ps1"
+Write-Output "------------------------------------------"
+$True
