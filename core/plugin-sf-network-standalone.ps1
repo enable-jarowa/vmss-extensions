69ee726530e0ad0670cc39a7fe48e962c11d8b44
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2022-11-16 11:44:59 +0100

    update vhosts + ports firewall mira

--- core/plugin-sf-network-standalone.ps1
+++ core/plugin-sf-network-standalone.ps1
@@ -34,6 +34,29 @@ if ($f_featurearray.Contains("sfstandalone")) {
         Write-Host "$($ruleName) - Rule already exists"
     }
 
+    $ruleName = "_Enable Service Fabric App Insights Ports"
+    $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
+    if ($null -eq $enableRule) {
+        New-NetFirewallRule -DisplayName "$($ruleName)" `
+            -Profile @('Domain', 'Private', 'Public') `
+            -Direction Inbound -Action Allow -Protocol TCP `
+            -LocalPort @('8052', '8052')
+    } else {
+        Write-Host "$($ruleName) - Rule already exists"
+    }
+
+    $ruleName = "_Enable Service Fabric App Mira Ports"
+    $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
+    if ($null -eq $enableRule) {
+        New-NetFirewallRule -DisplayName "$($ruleName)" `
+            -Profile @('Domain', 'Private', 'Public') `
+            -Direction Inbound -Action Allow -Protocol TCP `
+            -LocalPort @('8062', '8062')
+    } else {
+        Write-Host "$($ruleName) - Rule already exists"
+    }
+
+
 
     $ruleName = "_Enable Service Fabric App Health Ports"
     $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
--- core/plugin-sf-network.ps1
+++ core/plugin-sf-network.ps1
@@ -5,7 +5,7 @@ Write-Output "------------------------------------------"
 $ruleName = "_Enable Service Fabric App Ports"
 $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
 if ($null -eq $enableRule) {
-    New-NetFirewallRule -DisplayName '_Enable Service Fabric App Ports' `
+    New-NetFirewallRule -DisplayName $ruleName `
         -Profile @('Domain', 'Private', 'Public') `
         -Direction Inbound -Action Allow -Protocol TCP `
         -LocalPort @('8000', '8001', '8002', '8003', '8010')
@@ -16,7 +16,7 @@ if ($null -eq $enableRule) {
 $ruleName = "_Enable Service Fabric App Ports Special"
 $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
 if ($null -eq $enableRule) {
-    New-NetFirewallRule -DisplayName '_Enable Service Fabric App Ports Special' `
+    New-NetFirewallRule -DisplayName $ruleName `
         -Profile @('Domain', 'Private', 'Public') `
         -Direction Inbound -Action Allow -Protocol TCP `
         -LocalPort @('8008')
@@ -27,10 +27,33 @@ if ($null -eq $enableRule) {
 $ruleName = "_Enable Service Fabric App Config Ports"
 $enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
 if ($null -eq $enableRule) {
-    New-NetFirewallRule -DisplayName '_Enable Service Fabric App Config Ports' `
+    New-NetFirewallRule -DisplayName $ruleName `
         -Profile @('Domain', 'Private', 'Public') `
         -Direction Inbound -Action Allow -Protocol TCP `
         -LocalPort @('8020')
+} else {
+    Write-Host "$($ruleName) - Rule already exists"
+}
+
+$ruleName = "_Enable Service Fabric App Insights Ports"
+$enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
+if ($null -eq $enableRule) {
+    New-NetFirewallRule -DisplayName $ruleName `
+        -Profile @('Domain', 'Private', 'Public') `
+        -Direction Inbound -Action Allow -Protocol TCP `
+        -LocalPort @('8051', '8052')
+} else {
+    Write-Host "$($ruleName) - Rule already exists"
+}
+
+
+$ruleName = "_Enable Service Fabric App Mira Ports"
+$enableRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction Ignore
+if ($null -eq $enableRule) {
+    New-NetFirewallRule -DisplayName $ruleName `
+        -Profile @('Domain', 'Private', 'Public') `
+        -Direction Inbound -Action Allow -Protocol TCP `
+        -LocalPort @('8061', '8062')
 } else {
     Write-Host "$($ruleName) - Rule already exists"
 }
