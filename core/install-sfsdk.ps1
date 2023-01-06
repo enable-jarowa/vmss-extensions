9315621eac260485debc381c4bc3336484c525f2
Lorenz Hänggi JAROWA <lorenz.haenggi@jarowa.ch>
2023-01-06 13:27:38 +0100

    remove webpi installer

--- core/install-sfsdk.ps1
+++ core/install-sfsdk.ps1
@@ -12,31 +12,9 @@ $f_features=$args[5]
 Write-Output "Features=$($f_features)"
 $f_featurearray = $f_features.ToLower().Split(",").Trim().Where({ $_ -ne "" });
 
+## we still have to keep it for compat reasons
 if ($f_featurearray.Contains("sfsdk_NOTUSED")) {
-     Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Scope CurrentUser 
 
-    $msi = "WebPlatformInstaller_x64_en-US.msi"
-    $fileDownloaded = "$($env:TEMP)\$($msi)"
-    if (!(Test-Path $fileDownloaded -PathType leaf)) {
-        Invoke-WebRequest `
-            -Uri "https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x64_en-US.msi" `
-            -OutFile $fileDownloaded -UseBasicParsing
-    }
-    Start-Process "msiexec" -ArgumentList '/i', "$($fileDownloaded)", '/passive', '/quiet', '/norestart', '/qn' -NoNewWindow -Wait  -RedirectStandardOutput "$($env:TEMP)\WebPICMD.log"  -RedirectStandardError "$($env:TEMP)\WebPICMD.error.log" 
-
-    Write-Output "Installing /Products:ServiceFabricSDK_4_2_CU7"
-    Start-Sleep 10
-    Start-Process "$($env:programfiles)\microsoft\web platform installer\WebPICMD.exe" -ArgumentList '/Install', '/Products:"ServiceFabricSDK_4_2_CU7"', '/AcceptEULA', "/Log:$($env:TEMP)\WebPICMD-install-service-fabric-sdk.log" -NoNewWindow -Wait -RedirectStandardOutput "$($env:TEMP)\WebPICMD.log"  -RedirectStandardError "$($env:TEMP)\WebPICMD.error.log" 
-
-    Start-Sleep 10
-    Write-Output "Installing /Products:ServiceFabricRuntime_7_2_CU7"
-    Start-Process "$($env:programfiles)\microsoft\web platform installer\WebPICMD.exe" -ArgumentList '/Install', '/Products:"ServiceFabricRuntime_7_2_CU7"', '/AcceptEULA', "/Log:$($env:TEMP)\WebPICMD-install-service-fabric-runtime.log" -NoNewWindow -Wait -RedirectStandardOutput "$($env:TEMP)\WebPICMD.log"  -RedirectStandardError "$($env:TEMP)\WebPICMD.error.log" 
-
-    Write-Output "Installing /Products:ServiceFabricSDK_4_2_CU7 again"
-    Start-Sleep 10
-    Start-Process "$($env:programfiles)\microsoft\web platform installer\WebPICMD.exe" -ArgumentList '/Install', '/Products:"ServiceFabricSDK_4_2_CU7"', '/AcceptEULA', "/Log:$($env:TEMP)\WebPICMD-install-service-fabric-sdk.log" -NoNewWindow -Wait -RedirectStandardOutput "$($env:TEMP)\WebPICMD.log"  -RedirectStandardError "$($env:TEMP)\WebPICMD.error.log" 
-
-    Write-Output "installation done"
 
 } else {
     Write-Output "sfsdk - not installed"
